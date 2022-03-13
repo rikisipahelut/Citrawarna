1309cec336b9ffe04e7f7e1b26fbd6976efe4c58
rikisipahelut <rikisipahelut@gmail.com>
2022-03-13 19:41:43 +0800

    Penambahan dua file migrasi

---
+++ database/migrations/2022_03_13_110429_create_vendor_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVendorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vendor', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('kontak');
+            $table->string('status_pajak');
+            $table->string('bank');
+            $table->string('no_rekening');
+            $table->string('atas_nama');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendor');
+    }
+}
